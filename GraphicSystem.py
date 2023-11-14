74badccafbafe29a06a1468a85c7b79d39e9d5dd
Iuri <iurischmoeller@hotmail.com>
2023-11-14 17:26:44 -0300

    First commit

---
+++ GraphicSystem.py
@@ -0,0 +1,88 @@
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import time
+
+class GraphApp:
+    def __init__(self, master):
+        self.master = master
+        master.title("Graph Application")
+
+        self.figure, self.ax = Figure(), None
+        self.canvas = FigureCanvasTkAgg(self.figure, master=master)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        self.points = [(1, 1), (2, 2), (3, 1)]  # Exemplo de pontos
+
+        self.plot_points()
+
+        self.mouse_is_pressed = False
+        self.last_press = None
+
+        self.canvas.mpl_connect("button_press_event", self.on_mouse_press)
+        self.canvas.mpl_connect("motion_notify_event", self.on_mouse_drag)
+        self.canvas.mpl_connect("button_release_event", self.on_mouse_release)
+        self.canvas.mpl_connect("scroll_event", self.on_mouse_scroll)
+
+    def plot_points(self):
+        if self.ax:
+            self.ax.clear()
+
+        self.ax = self.figure.add_subplot(111)
+
+        x, y = zip(*self.points)
+        self.ax.scatter(x, y)
+
+        self.ax.grid(True, linestyle='-', alpha=0.5) 
+
+        self.ax.axhline(0, color='black', lw=2)  # Linha horizontal no eixo x
+        self.ax.axvline(0, color='black', lw=2)  # Linha vertical no eixo y
+
+
+        self.canvas.draw()
+
+    def on_mouse_press(self, event):
+        # Move o plano cartesiano com o mouse
+        if event.inaxes:
+            self.last_press = (event.xdata, event.ydata)
+        self.mouse_is_pressed = True
+
+    def on_mouse_release(self, event):
+        self.mouse_is_pressed = False
+
+    def on_mouse_drag(self, event):
+        if not self.last_press or not self.mouse_is_pressed:
+            return
+
+        # Move o plano cartesiano com o mouse
+        if self.last_press and event.inaxes:
+            dx = event.xdata - self.last_press[0]
+            dy = event.ydata - self.last_press[1]
+
+            dx, dy = dx*0.7, dy*0.7
+
+            xlim = self.ax.get_xlim()
+            ylim = self.ax.get_ylim()
+
+            self.ax.set_xlim(xlim[0] - dx, xlim[1] - dx)
+            self.ax.set_ylim(ylim[0] - dy, ylim[1] - dy)
+
+            self.last_press = (event.xdata, event.ydata)
+            self.canvas.draw()
+
+    def on_mouse_scroll(self, event):
+        # Dá zoom no plano cartesiano com o scroll do mouse
+        if event.inaxes:
+            if event.step < 0:
+                self.ax.set_xlim(self.ax.get_xlim()[0], self.ax.get_xlim()[1] * 1.1)
+                self.ax.set_ylim(self.ax.get_ylim()[0], self.ax.get_ylim()[1] * 1.1)
+            else:
+                self.ax.set_xlim(self.ax.get_xlim()[0], self.ax.get_xlim()[1] / 1.1)
+                self.ax.set_ylim(self.ax.get_ylim()[0], self.ax.get_ylim()[1] / 1.1)
+
+            self.canvas.draw()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GraphApp(root)
+    root.mainloop()
